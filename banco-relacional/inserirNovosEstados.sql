bd2a51c4e69873bf8f4a397d58c6fc9177aa78a0
Matheus <matheus.reis13@hotmail.com>
2019-12-11 23:29:06 -0300

    DOM(Integração)

---
+++ banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,13 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+select * from estados
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Mais Novo', 'MN', 'Norte', 2.51)
+
+insert into estados (id,nome, sigla, regiao, populacao)
+values ('28','Teste', 'PT', 'Norte', 2.51)
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Teste2', 'PL', 'Norte', 2.50)
